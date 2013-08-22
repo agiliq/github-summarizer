e2328dff057672f39568571fa433aa85fc33c0da
krvc <krvc.it@gmail.com>
2013-08-22 23:02:03 +0530

    Added mailinglist file

---
+++ mailing_list.py
@@ -0,0 +1,24 @@
+email_to = {'ramana@agiliq.com': {'full_name': 'Venkata Ramana C',
+                                  'login': 'arjunc77'},
+
+            'dheeru@agiliq.com': {'full_name': 'Dheeraj Kumar Ketireddy',
+                                  'login': 'dheeru0198'},
+
+            'akshar@agiliq.com': {'full_name': 'Akshar Raaj',
+                                  'login': 'akshar-raaj'},
+
+            'balu@agiliq.com': {'full_name': 'Bala Subrahmanyam Varanasi',
+                                'login': 'Balu-Varanasi'},
+
+            'shabda@agiliq.com': {'full_name': 'Shabda Raaj',
+                                  'login': 'shabda'},
+
+            'bhaskar@agiliq.com': {'full_name': 'Bhaskar Rao',
+                                   'login': 'bhaskar'},
+
+            'rakesh@agiliq.com': {'full_name': 'Rakesh Vidya Chandra',
+                                  'login': 'krvc'},
+            'shiva@agiliq.com': {'full_name': 'Shiva Krishna',
+                                 'login': 'shivakrshn49'},
+            'imohammed23@gmail.com': {'full_name': 'Imran Mohammed',
+                                      'login': 'mipreamble'}}
