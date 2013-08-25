91181b123c23de42e3f969f74e7ebd1596180cb3
krvc <krvc.it@gmail.com>
2013-08-26 01:06:27 +0530

    Modified mailing list

--- mailing_list.py
+++ mailing_list.py
@@ -1,24 +1,28 @@
-email_to = {'ramana@agiliq.com': {'full_name': 'Venkata Ramana C',
-                                  'login': 'arjunc77'},
+# email_to = {'ramana@agiliq.com': {'full_name': 'Venkata Ramana C',
+#                                   'login': 'arjunc77'},
 
-            'dheeru@agiliq.com': {'full_name': 'Dheeraj Kumar Ketireddy',
-                                  'login': 'dheeru0198'},
+#             'dheeru@agiliq.com': {'full_name': 'Dheeraj Kumar Ketireddy',
+#                                   'login': 'dheeru0198'},
 
-            'akshar@agiliq.com': {'full_name': 'Akshar Raaj',
-                                  'login': 'akshar-raaj'},
+#             'akshar@agiliq.com': {'full_name': 'Akshar Raaj',
+#                                   'login': 'akshar-raaj'},
 
-            'balu@agiliq.com': {'full_name': 'Bala Subrahmanyam Varanasi',
-                                'login': 'Balu-Varanasi'},
+#             'balu@agiliq.com': {'full_name': 'Bala Subrahmanyam Varanasi',
+#                                 'login': 'Balu-Varanasi'},
 
-            'shabda@agiliq.com': {'full_name': 'Shabda Raaj',
-                                  'login': 'shabda'},
+#             'shabda@agiliq.com': {'full_name': 'Shabda Raaj',
+#                                   'login': 'shabda'},
 
-            'bhaskar@agiliq.com': {'full_name': 'Bhaskar Rao',
-                                   'login': 'bhaskar'},
+#             'bhaskar@agiliq.com': {'full_name': 'Bhaskar Rao',
+#                                    'login': 'bhaskar'},
 
-            'rakesh@agiliq.com': {'full_name': 'Rakesh Vidya Chandra',
-                                  'login': 'krvc'},
-            'shiva@agiliq.com': {'full_name': 'Shiva Krishna',
-                                 'login': 'shivakrshn49'},
-            'imohammed23@gmail.com': {'full_name': 'Imran Mohammed',
-                                      'login': 'mipreamble'}}
+#             'rakesh@agiliq.com': {'full_name': 'Rakesh Vidya Chandra',
+#                                   'login': 'krvc'},
+#             'shiva@agiliq.com': {'full_name': 'Shiva Krishna',
+#                                  'login': 'shivakrshn49'},
+#             'imohammed23@gmail.com': {'full_name': 'Imran Mohammed',
+#                                       'login': 'mipreamble'},
+#             'tuxcanfly@gmail.com': {'full_name': 'Jakh Daven',
+#                                     'login': 'tuxcanfly'}}
+
+email_to = {'rakesh@agiliq.com': {'full_name': 'Rakesh Vidya Chandra', 'login': 'krvc'}}
