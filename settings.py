4e60863a1de59d5067304b6274a0b60d2aa5c1a6
krvc <krvc.it@gmail.com>
2013-08-26 01:08:38 +0530

    Modified settings to use get env_variable

--- settings.py
+++ settings.py
@@ -1,12 +1,22 @@
 ####################################################################
 #                      Summarizer Configurations                   #
 ####################################################################
+import os
+
+
+def get_env_variable(var_name):
+    try:
+        return os.environ[var_name]
+    except KeyError, e:
+        print 'Got keyError -reason "%s"' str(e)
+
+username = get_env_variable('username')
+password = get_env_variable('username')
+sendgrid_auth = get_env_variable('sendgrid_auth')
+ORGANIZATION = get_env_variable('ORGANIZATION')
 
 # mailing configurations
-SENDER = "rakesh@agiliq.com"
-SUBJECT = "Agiliq-Github Summary for the day "
-
-ORGANIZATION = 'agiliq'
-
+SENDER = get_env_variable('SENDER')
+SUBJECT = get_env_variable('SUBJECT')
 
 ####################################################################
